41e829a122d00b0a6ffc1cdafd2e92562761202f
viktoriyaloshkarova <viktoriyaloshkarova0419@gmail.com>
2021-06-15 09:53:50 -0400

    Wrote "Hello" on website

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceworker';
 
 render(<App />, document.getElementById('root'));
 
