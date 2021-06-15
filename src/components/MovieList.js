de8a96afe1c589d6630d89b595cfbd276fc4bc13
viktoriyaloshkarova <viktoriyaloshkarova0419@gmail.com>
2021-06-15 11:39:53 -0400

    Hard coded movie info to test API

---
+++ src/components/MovieList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MovieList = (props) => {
+    return (
+        <>
+        {props.movies.map((movie, index)=> (
+            <div>
+                <p src={movie.Title}> </p>
+                <img src={movie.Poster} alt = 'movie'></img>
+            </div>
+        ))}
+        </>
+    );
+};
+
+export default MovieList;
