26b7e49c450a68f046f1d5351991fa9300090ffe
viktoriyaloshkarova <viktoriyaloshkarova0419@gmail.com>
2021-06-17 10:37:02 -0400

    Created a movie file to get info from each movie

---
+++ src/components/Movie.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Movie ({ result }){
+    return (
+        <div className = 'movie'>
+            <h3>{movie.Title}</h3>
+        </div>
+    )
+}
+
+export default Movie;
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,33 +1,16 @@
 import React from 'react';
 import Popup from './Popup';
-import { useState } from 'react';
+import Movie from './Movie'
 
-const MovieList = (props) => {
-    
-    const [buttonPopup, setButtonPopup] = useState(false);
+function MovieList ({ results }) {
     
     return (
-        <>
-        {props.movies.map((movie, index)=> (
-            <div className = 'movie'>
-                <main>
-                <button onClick = {() => setButtonPopup(true)} className = 'title'>{movie.Title} </button>
-                </main>
+        <section className = 'movies'>
+            {MovieList.map(movie => (
+                <Movie movie = {movie}/>
+            ))}
 
-                <Popup trigger = {buttonPopup} setTrigger = {setButtonPopup}>
-                    <h3>{movie.Title}</h3>
-                    <p>Release date: {movie.Released} </p>
-                    <p> Runtime: {movie.Runtime}</p>
-                    <p>Genre: </p>
-                    <p>Director: {movie.Director}</p>
-                </Popup>
-                <br/><br/>
-                <div className = 'poster'>
-                    <img src={movie.Poster} alt = 'movie'></img>
-                </div>
-            </div>
-        ))}
-        </>
+        </section>
     )
 }
 
